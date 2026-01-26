1aeb7f606a55433532507ed91fa1001281376a65
junseokhub <github.yam117@passinbox.com>
2026-01-26 21:32:23 +0900

    Feat: signUp Controller

--- src/main/java/com/subscription/plan/controller/MemberController.java
+++ src/main/java/com/subscription/plan/controller/MemberController.java
@@ -1,6 +1,12 @@
 package com.subscription.plan.controller;
 
+import com.subscription.plan.domain.Member;
+import com.subscription.plan.dto.MemberSignUpRequestDto;
+import com.subscription.plan.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
 
+    @PostMapping()
+    public ResponseEntity<Member> signUp(@RequestBody MemberSignUpRequestDto memberSignUpRequestDto) {
+        Member member = memberService.saveMember(memberSignUpRequestDto);
+        return ResponseEntity.ok(member);
+    }
 }
